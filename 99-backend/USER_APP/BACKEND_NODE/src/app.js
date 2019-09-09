d49e3b7ebbbeb9c706b620084f16abe1cd496e58
Darwin Felipe Uzcategui Gonzalez <Darwin.Uzcategui1973@hotmail.com>
2019-09-08 20:00:03 -0400

    backend mongo y mysql

---
+++ 99-backend/USER_APP/BACKEND_NODE/src/app.js
@@ -0,0 +1,17 @@
+const  express = require('express');
+const app =  express();
+
+
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(require('./rutas/users'));
+
+
+module.exports = app;
+
+
+
---
+++ 99-backend/USER_APP/BACKEND_NODE/src/rutas/users.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const rutas =Router();
+
+
+
+
+module.exports = rutas;
