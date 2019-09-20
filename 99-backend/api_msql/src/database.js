15f47238af33ac3e361deb37ee4493ab5998d2fa
Darwin Uzcategui <darwin.uzcategui1973@hotmail.com>
2019-09-20 16:52:13 -0400

    Creacion angular-electron

--- 99-backend/api_msql/src/database.js
+++ 99-backend/api_msql/src/database.js
@@ -2,18 +2,18 @@ const mysql = require('mysql');
 const mysqlConexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Gmd123456**',
     database: 'nomina'
 });
 
-mysqlConexion.connect( function (err) {
-    if(err) {
+mysqlConexion.connect(function(err) {
+    if (err) {
         console.log(err);
         return;
-        
+
     } else {
         console.log('Base de datos conectada');
-        
+
     }
 
 });
